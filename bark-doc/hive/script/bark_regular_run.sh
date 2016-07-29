58a2ba98c454f90a7f2e75aefa2a0c79c6a60422
Stanley Shi <deming.shi@gmail.com>
2016-07-29 14:04:43 +0800

    finding root_dir instead of hard coded dir;

--- bark-doc/hive/script/bark_regular_run.sh
+++ bark-doc/hive/script/bark_regular_run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+ROOT_DIR=$(cd $(dirname $0); pwd)
+
+SLEEP_INTERVAL=${SLEEP_INTERVAL:-60}
+
+if [ ! -f $ROOT_DIR/bark_jobs.sh ]; then
+  echo "bark_jobs.sh not found!"
+  exit 1
+fi
+
 set +e
 while true
 do
-  echo "start"
-  /home/hduser/bark/bark_jobs.sh 2>&1
-  rcode=$?
-  echo "end $rcode"
-  rm -rf /home/hduser/bark/nohup.out
-  sleep 60
+  $ROOT_DIR/bark_jobs.sh 2>&1
+  sleep $SLEEP_INTERVAL
 done
 set -e
